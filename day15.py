dc1f667a6ba8dee7b7ee60124dd40a8157996f01
bwigianto <bwigianto@gmail.com>
2017-12-15 18:33:51 -0600

    Update day15.py

--- day15.py
+++ day15.py
@@ -8,6 +8,19 @@ def generate_a(prev):
 def generate_b(prev):
   return generate(prev, 48271)
 
+def generator_func(x, n, gen):
+  prev = x
+  for i in range(n):
+    curr = gen(prev)
+    prev = curr
+    yield curr
+
+def generator_a(x, n):
+  return generator_func(x, n, generate_a)
+
+def generator_b(x, n):
+  return generator_func(x, n, generate_b)
+
 def lowest16(n):
   return "{0:016b}".format(n)[-16:]
 
@@ -25,7 +38,14 @@ def count_matches(n):
     b = generate_b(b)
   return ct
 
+def count_matches2(n):
+  
+
 def part1():
   print(count_matches(40000000))
 
 def part2():
+  return [x for x in generator_a(65, 10)]
+  
+
+print(part2())
